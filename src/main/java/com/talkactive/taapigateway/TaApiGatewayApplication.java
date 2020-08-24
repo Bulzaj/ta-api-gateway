5d67355273661cd574e3313affcd76100f4fe9c1
bulzaj <ernestsado94@gmail.com>
2020-08-24 17:27:42 +0200

    first commit

---
+++ src/main/java/com/talkactive/taapigateway/TaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.talkactive.taapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talkactive/taapigateway/TaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkactive.taapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
