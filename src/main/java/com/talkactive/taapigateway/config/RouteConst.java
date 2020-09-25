2c707a79d1f0611c4351a95f3eca3f21d36e0963
bulzaj <ernestsado94@gmail.com>
2020-09-25 12:53:09 +0200

    refactor: code cleanup

--- src/main/java/com/talkactive/taapigateway/config/RouteConst.java
+++
@@ -1,7 +0,0 @@
-package com.talkactive.taapigateway.config;
-
-public class RouteConst {
-
-    public static final String USER_DETAILS_SERVICE = "http://localhost:8000/api/user-details";
-    public static final String MESSAGES_EXCHANGE_SERVICE = "http://localhost:8010";
-}
